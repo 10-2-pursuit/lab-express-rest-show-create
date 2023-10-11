df912bc14fb7defc5dfa2e9dbcff89007193f673
Shanice <shanicegriffin@pursuit.org>
2023-10-11 18:06:14 -0400

    fixed file name

--- controllers/log.controller.js
+++ controllers/log.controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const logs = express.Router();
 
-let logsArray = require("../models/log.model.js");
+let logsArray = require("../models/log.js");
 
 logs.get("/", (req, res) => {
     res.send(logsArray);
