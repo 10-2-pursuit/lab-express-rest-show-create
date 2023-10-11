1fa506ed70ad342017929321c5119493a6dd3fe7
Shanice <shanicegriffin@pursuit.org>
2023-10-11 19:22:24 -0400

    passed all tests

--- controllers/log.controller.js
+++ controllers/log.controller.js
@@ -8,4 +8,24 @@ logs.get("/", (req, res) => {
     res.send(logsArray);
 })
 
+logs.get("/:index", (req, res) => {
+    const {index} = req.params;
+    if(logsArray[index]) {
+        res.status(200).send(logsArray[index]);
+    } else {
+        res.redirect("/")
+    }
+})
+
+logs.post("/", (req, res) => {
+    logsArray.push(req.body);
+    res.status(200).send(logsArray[logsArray.length - 1])
+})
+
+logs.delete("/:index", (req, res) => {
+    const {index} = req.params;
+    logsArray.splice(index, 1);
+    res.status(200).send(logsArray)
+})
+
 module.exports = logs;
