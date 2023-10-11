2c9169ce7b6bf3cc9e4bc2da29de3fde95ce976a
arim83 <jorammercado@pursuit.org>
2023-10-11 04:59:58 -0400

    show with redirect complete

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const app = express()
 
 app.use(express.json())
 
+// app.use((req,res, next) => {
+//     //res.status(404).send("invalid index")
+//     next()
+// })
+
 app.get("/", (req, res) => {
   res.send("welcome to the captain's log")
 })
--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -6,21 +6,16 @@ logs.get("/", (req,res) => {
     res.json(logsArray)
 })
 
-logs.post("/", (req,res) => {
-    //console.log("hi")
-    logsArray.push(req.body)
-    res.status(200).json({status: "OK", payload: logsArray[logsArray.length-1]   })
-    //console.log(logsArray.length)
-    //console.log(logsArray)
+logs.get("/:arrayIndex", (req,res) => {
+   if(logsArray[req.params.arrayIndex])
+        res.json(logsArray[req.params.arrayIndex])
+    else
+        res.redirect('/9001')
 })
 
-logs.get("/:arrayIndex", (req,res) => {
-   if(logsArray[req.params.arrayIndex]){
-        res.json(logsArray[req.params.arrayIndex])
-   }
-    else{
-       res.status(404).json({error: "invalid index"})
-    }
+logs.post("/", (req,res) => {
+    logsArray.push(req.body)
+    res.status(200).json({status: "OK", payload: logsArray[logsArray.length-1]})
 })
 
 module.exports = logs
