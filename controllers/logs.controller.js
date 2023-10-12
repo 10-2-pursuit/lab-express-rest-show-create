769019d295cd5f64b8d76830e64d64dad7411e1f
Jalal Jonaid <jalaljonaid@jalals-air.myfiosgateway.com>
2023-10-11 21:03:44 -0400

    Fixes test with GET

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -15,13 +15,26 @@ logs.get("/:id", (req, res) => {
     if(logsData[id]) {
         res.status(200).json(logsData[id])
     } else {
-        res.status(404).send("No log for this ID.");
+        ///res.status(404).send("No log for this ID.");
+        res.redirect('/');
     }
 })
 
 logs.post("/", (req, res) => {
     logsData.push(req.body);
     res.status(200).json( {status: "OK", payload: logsData[logsData.length - 1]} )
+})
+
+logs.delete("/:logIndex", (req, res) => {
+    const { logIndex } = req.params;
+
+    if (logsData[logIndex]) {
+        logsData[logIndex] = req.body;
+        const deletedLog = logsData.splice(logIndex, 1);
+        res.status(200).json((logsData[logIndex]))
+    } else {
+        res.status(404).send("No log for this ID.");
+    }
 })
 
 module.exports = logs;
