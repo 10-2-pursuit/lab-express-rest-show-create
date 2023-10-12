a87536cc19297dd71dd4ff086881e2bb198fd723
Shanice <shanicegriffin@pursuit.org>
2023-10-11 20:28:40 -0400

    added a 404 route

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const logs = require("./controllers/log.controller.js")
+const logs = require("./controllers/log.controller.js");
 
 const app = express();
 
@@ -8,7 +8,11 @@ app.use(express.json());
 app.use("/logs", logs);
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the captain's log!")
-})
+  res.send("Welcome to the captain's log!");
+});
+
+app.use("/404", (req, res) => {
+  res.status(404).send("No logs at that index exist.");
+});
 
 module.exports = app;
--- controllers/log.controller.js
+++ controllers/log.controller.js
@@ -5,27 +5,43 @@ const logs = express.Router();
 let logsArray = require("../models/log.js");
 
 logs.get("/", (req, res) => {
-    res.send(logsArray);
-})
+  res.send(logsArray);
+});
 
 logs.get("/:index", (req, res) => {
-    const {index} = req.params;
-    if(logsArray[index]) {
-        res.status(200).send(logsArray[index]);
-    } else {
-        res.redirect("/")
-    }
-})
+  const { index } = req.params;
+  if (logsArray[index]) {
+    res.status(200).send(logsArray[index]);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 logs.post("/", (req, res) => {
-    logsArray.push(req.body);
-    res.status(200).send(logsArray[logsArray.length - 1])
-})
+  logsArray.push(req.body);
+  res.status(200).send(logsArray[logsArray.length - 1]);
+});
 
 logs.delete("/:arrayIndex", (req, res) => {
-    const {arrayIndex} = req.params;
+  const { arrayIndex } = req.params;
+
+  if (logsArray[arrayIndex]) {
     const deletedLog = logsArray.splice(arrayIndex, 1);
-    res.status(200).send(deletedLog[0])
-})
+    res.status(200).send(deletedLog[0]);
+  } else {
+    res.redirect("/404");
+  }
+});
+
+logs.put("/:arrayIndex", (req, res) => {
+  const { arrayIndex } = req.params;
+
+  if (logsArray[arrayIndex]) {
+    logsArray[arrayIndex] = req.body;
+    res.status(200).send(logsArray[arrayIndex]);
+  } else {
+    res.redirect("/404");
+  }
+});
 
 module.exports = logs;
