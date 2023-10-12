9dc89e6bd94ab24e5bf28bcc816b6b6afdb22391
Shanice <shanicegriffin@pursuit.org>
2023-10-11 22:30:54 -0400

    all test passed for pt 1 and 2, with bonus of part 1

--- controllers/log.controller.js
+++ controllers/log.controller.js
@@ -5,6 +5,44 @@ const logs = express.Router();
 let logsArray = require("../models/log.js");
 
 logs.get("/", (req, res) => {
+  const { order, mistakes, lastCrisis } = req.query;
+  let sortedLogs = [...logsArray];
+
+  if (order) {
+    if (req.query.order === "asc") {
+      sortedLogs.sort((a, b) => a.captainName.localeCompare(b.captainName));
+      res.send(sortedLogs);
+    } else if (req.query.order === "desc") {
+      sortedLogs.sort((a, b) => b.captainName.localeCompare(a.captainName));
+      res.send(sortedLogs);
+    }
+  }
+
+  if (mistakes) {
+    if (req.query.mistakes === "true") {
+      sortedLogs = sortedLogs.filter(
+        (log) => log.mistakesWereMadeToday === true
+      );
+      res.send(sortedLogs);
+    } else if (req.query.mistakes === "false") {
+      sortedLogs = sortedLogs.filter(
+        (log) => log.mistakesWereMadeToday === false
+      );
+      res.send(sortedLogs);
+    }
+  }
+  if (lastCrisis) {
+    if (req.query.lastCrisis === "gt10") {
+      sortedLogs = sortedLogs.filter((log) => log.daysSinceLastCrisis > 10);
+      res.send(sortedLogs);
+    } else if (req.query.lastCrisis === "gte20") {
+      sortedLogs = sortedLogs.filter((log) => log.daysSinceLastCrisis >= 20);
+      res.send(sortedLogs);
+    } else if (req.query.lastCrisis === "lte5") {
+      sortedLogs = sortedLogs.filter((log) => log.daysSinceLastCrisis <= 5);
+      res.send(sortedLogs);
+    }
+  }
   res.send(logsArray);
 });
 
