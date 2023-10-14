f72b5f37d82d950d12db1fa22424f4dc357f14ea
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-10-14 01:39:59 -0400

    passing all tests

--- app.js
+++ app.js
@@ -8,18 +8,18 @@ app.use("/logs", logs);
 
 app.use(express.urlencoded({extended: true}));
 
-app.use((err, req, res, next)=> {
- if(err) {
-    if (err.status === 404){
-        res.status(404).send('Not Found');
-    } else if (err.name === "ValidationError"){
-        res.status(400).json({error: "Validation failed", details: err.details });
-    } else {
-        res.status(500).send("internal Server Error");
-    }
+// app.use((err, req, res, next)=> {
+//  if(err) {
+//     if (err.status === 404){
+//         res.status(404).send('Not Found');
+//     } else if (err.name === "ValidationError"){
+//         res.status(400).json({error: "Validation failed", details: err.details });
+//     } else {
+//         res.status(500).send("internal Server Error");
+//     }
 
-    } else{
-        next();
-    }
-})
+//     } else{
+//         next();
+//     }
+// })
 module.exports = app
