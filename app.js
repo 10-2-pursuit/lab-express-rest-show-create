9837efb195b023a7ea53b73b416c7f2fcf8344a7
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-10-13 23:38:04 -0400

    created logic for express url encoded

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const logs = require("./controllers/logscontroller");
 const app = express(); 
 
 app.use(express.json());
+app.use(express.urlencoded({exended: true}));
 app.use("/logs", logs);
 
+app.use((err, req, res, next)=> {
+ if(err) {
+    if (err.status === 404){
+        res.status(404).send('Not Found');
+    } else if (err.name === "ValidationError"){
+        res.status(400).json({error: "Validation failed", details: err.details });
+    } else {
+        res.status(500).send("internal Server Error");
+    }
+
+    } else{
+        next();
+    }
+})
 module.exports = app
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app.js")
 
-require("dotenv").config() 
-const PORT= process.env.PORT 
+require("dotenv").config();
+const PORT= process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT} `)
