a299b6208bb859358c0e451fb84af04618a50df1
Joseph-Carter <1joseph1carter@gmail.com>
2023-12-06 11:49:17 -0500

    fixed error path

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const express = require("express")
 const captainLogs = require("./controllers/captainLogsContoller")
 
 const app = express();
+const cors = require("cors");
 
+app.use(cors())
 app.use(express.json())
+
 // this is where we use CORS
 app.use("/logs", captainLogs);
 
@@ -12,11 +15,8 @@ app.get("/", (req, res) => {
     res.send("Welcome to the captain's log")
 })
 
-app.get("/404", (req, res) => {
-    res.send(`#	Action	URL	HTTP Verb	CRUD	Description
-    2	Show	/logs/:id	GET	Read	Get an individual view (show one log)
-    3	Create	/logs	POST	Create	Create a new log
-    `)
+app.get("*", (req, res) => {
+    res.status(404).json({error: "no page found "})
 })
 
 module.exports = app;
